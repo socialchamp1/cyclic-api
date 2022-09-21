f148122bfd2a178daf63860183467354e053657b
vallesty <mike.vallesty@gmail.com>
2022-09-22 06:43:02 +0700

    aa

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const appRoot = require('app-root-path')
 const reqlib = require('app-root-path').require
 require('dotenv').config({ path: appRoot + '/.env' })
 
-const main = async() => {
-    try{
-        console.log('Good morning Mike')
-    }
-    catch(e) { console.log(e) }
-}
-
-main()
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`)
+    res.write('Yo!');
+    res.end();
+}).listen(process.env.PORT || 3000);
